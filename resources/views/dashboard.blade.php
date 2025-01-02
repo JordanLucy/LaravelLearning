c2120797efea2305fb90d536cd5514c8c214895f
jordanlucy <jord_lucy_98@outlook.com>
2025-01-02 17:26:51 +0000

    Testing initial changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,10 @@
 <x-app-layout>
-<div class="container">
-    <div class="row">
-        <div class="col-3">
-
+    <div class="grid grid-cols-2 gap-4">
+        <div class="col-span-1">
+            <img src="/imagery/TlhCrimboLogo.svg" class="rounded-full" >
         </div>
-        <div class="col-9">
-            <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F596515913138391716%2F&psig=AOvVaw1ExsM2yMchTatikCbA5AQB&ust=1735919917326000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJja8-Oz14oDFQAAAAAdAAAAABAE" class="rounded-circle">
+        <div class="col-span-1">
+            <p class="bg-blue-500"> Testing it is working</p>
         </div>
     </div>
-
-
-</div>
 </x-app-layout>
