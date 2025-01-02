75c96efa134cd216dc9883b3a1f79c16b83a674f
jordanlucy <jord_lucy_98@outlook.com>
2025-01-02 16:08:12 +0000

    Testing initial changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,14 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<div class="container">
+    <div class="row">
+        <div class="col-3">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
+        </div>
+        <div class="col-9">
+            <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F596515913138391716%2F&psig=AOvVaw1ExsM2yMchTatikCbA5AQB&ust=1735919917326000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJja8-Oz14oDFQAAAAAdAAAAABAE" class="rounded-circle">
         </div>
     </div>
+
+
+</div>
 </x-app-layout>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,14 +6,17 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+{{--                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />--}}
+                        <div><img src="/imagery/TlhCrimboLogo.svg" style="height: 50px; width: 50px; margin-right: 10px;"></div>
+
                     </a>
+                    <div class="white-line" style=" height: 50px; border-right: 1px solid #fff"></div>
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <div class="hidden space-x-8 sm:-my-px sm:ms-6 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Testing Dashboard') }}
                     </x-nav-link>
                 </div>
             </div>
